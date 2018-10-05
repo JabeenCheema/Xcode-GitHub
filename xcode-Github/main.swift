4a167da204114778c682dffa550bc769ac5a8abc
Jabeen Cheema <jabeencheema@pursuit.org>
2018-10-05 15:07:31 -0400

    added todos for code practice

--- xcode-Github/main.swift
+++ xcode-Github/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+// control flow
 
+let day = "Friday"
+
+if day == "Friday" {
+    print("the weekend is here")
+} else {
+    print("pursuit happiness")
+}
+
+//ToDo: practice control flow 
+
+// ToDo: practice writing switch statements
+
+//ToDo: practice writing tuples
